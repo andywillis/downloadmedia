871af117cfda9e74ab3d1d220284706651f7f731
Andy Willis <awillis@fastmail.fm>
2022-04-29 09:11:32 +0100

    Bump to v1.9.1

--- rundev.sh
+++ rundev.sh
@@ -1,1 +1,1 @@
-web-ext run --firefox="/c/Program Files/Firefox Developer Edition/firefox.exe" --firefox-profile="/c/Users/Andy/AppData/Roaming/Mozilla/Firefox/Profiles/5xybitde.default"
+web-ext run --firefox="/c/Program Files/Firefox Developer Edition/firefox.exe" --firefox-profile="/c/Users/accou/AppData/Roaming/Mozilla/Firefox/Profiles/svsh6t5r.Dev"
